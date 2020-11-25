4bd30633012a0a4c49b66e7a173ba57741420fc9
Pavlo O. Melnyk <38461499+pavlo-melnyk@users.noreply.github.com>
2020-11-26 00:02:45 +0100

    load train data; add object attributes to vertices

--- notebooks/Scene-Graph-Motifs.py
+++ notebooks/Scene-Graph-Motifs.py
@@ -9,9 +9,12 @@
 
 # MAGIC %md ##Graph structure
 # MAGIC 
-# MAGIC - We want to extract the names of objects we see in the images ans use their id's as vertices (create a object_name2id dictionary)
-# MAGIC - For one object category, we will have multiple id's, but only one vertex in the graph (coarse-graph representation);
-# MAGIC - The edge properties are the names of the relations in each .json file; we extract them and store in triples (?) and create edge_name2id.
+# MAGIC - We want to extract the names of objects we see in the images and use their id's as vertices (create an object2id dictionary).
+# MAGIC - For one object category, we will have multiple id's, but only one vertex in the graph (coarse-graph representation).
+# MAGIC - The edge properties are the names of the relations in each .json file; we extract them and store in triples (?) and create edge2id.
+# MAGIC 
+# MAGIC 
+# MAGIC - Object attributes may be used as part of the vertices
 
 # COMMAND ----------
 
@@ -90,39 +93,51 @@ import json
 
 # COMMAND ----------
 
+# load train and validation graph data:
+f_train = open("/dbfs/FileStore/shared_uploads/scenegraph_motifs/train_sceneGraphs.json")
+train_scene_data = json.load(f_train)
+
 f = open("/dbfs/FileStore/shared_uploads/scenegraph_motifs/val_sceneGraphs.json")
 val_scene_data = json.load(f)
 
 # COMMAND ----------
 
-
-# Pythonic way of doing it, must be converted to dataframe processing
-##### Pavlo's comment: the pythonic way may be a not bad option https://stackoverflow.com/questions/39818368/convert-lines-of-json-in-rdd-to-dataframe-in-apache-spark
-def json_to_vertices_edges(graph_json):
+# Pythonic way of doing it:
+def json_to_vertices_edges(graph_json, include_object_attributes=False):
   vertices = []
   edges = []
+  
   vertice_ids = graph_json['objects']
   for vertice_id in vertice_ids:
     vertice_obj = graph_json['objects'][vertice_id]
     name = vertice_obj['name']
-    vertices.append((vertice_id, name))
+    vertices_data = [vertice_id, name]
+    if include_object_attributes:
+      attributes = vertice_obj['attributes']  
+      vertices_data.append(attributes)      
+    vertices.append(tuple(vertices_data))
+    
     for relation in vertice_obj['relations']:
         src = vertice_id
         dst = relation['object']
         name = relation['name']
         edges.append((src, dst, name))
+        
   return (vertices, edges)
 
 # COMMAND ----------
 
-# Pythonic way of doing it, must be converted to dataframe processing
+# Pythonic way of doing it:
 def parse_scene_graphs(scene_graphs_json, vertice_schema, edge_schema):
   
   vertices = []
   edges = []
   
+  # if vertice_schema has a field for attributes:
+  include_object_attributes = len(vertice_schema) == 3
+     
   for scene_graph_id in scene_graphs_json:
-    vs, es = json_to_vertices_edges(scene_graphs_json[scene_graph_id])
+    vs, es = json_to_vertices_edges(scene_graphs_json[scene_graph_id], include_object_attributes)
     vertices += vs
     edges += es
     
@@ -133,33 +148,63 @@ def parse_scene_graphs(scene_graphs_json, vertice_schema, edge_schema):
 
 # COMMAND ----------
 
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql.types import StructType, StructField, ArrayType, IntegerType, StringType
 # Create schemas for scene graphs
 vertice_schema = StructType([
   StructField("id", StringType(), False), StructField("object_name", StringType(), False)
 ])
-  
+
+# vertices including attributes:
+# vertice_schema_with_attr  = StructType([
+#   StructField("id", StringType(), False), 
+#   StructField("object_name", StringType(), False), 
+#   # nested field with attributes (considering at most 3 attributes)
+#   StructField("attributes", StructType([
+#                StructField('attr_1', StringType(), True),
+#                StructField('attr_2', StringType(), True),
+#                StructField('attr_3', StringType(), True)
+#              ]), True)
+# ])
+
+# or better yet
+vertice_schema_with_attr  = StructType([
+  StructField("id", StringType(), False), 
+  StructField("object_name", StringType(), False), 
+  # might want to check ArrayType examples here: https://sparkbyexamples.com/spark/spark-array-arraytype-dataframe-column/
+  StructField("attributes", ArrayType(StringType()), True)
+])
+
 edge_schema = StructType([
   StructField("src", StringType(), False), StructField("dst", StringType(), False), StructField("relation_name", StringType(), False)
 ])
 
 # COMMAND ----------
 
-# MAGIC %md ### TODO - add attribute, and types to edges, to the graph structure
+# will use the length of the vertice schemas to parse the graph from the json files appropriately:
+len(vertice_schema), len(vertice_schema_with_attr)
+
+# COMMAND ----------
+
+# MAGIC %md ### TODO - add types to edges in the graph structure
 # MAGIC 
 # MAGIC We can do more interesting queries if the edges disclose what type/name the source and destination has. For instance, it is then possible to group the edges not by the ID by by which type of objects they are connected to, answering questions like how often is objects of type 'person' in the relation 'next-to' objects of type 'banana'.
 
 # COMMAND ----------
 
-scene_graphs = parse_scene_graphs(val_scene_data, vertice_schema, edge_schema)
+# scene_graphs = parse_scene_graphs(val_scene_data, vertice_schema, edge_schema)
+
+# train_scene_graphs = parse_scene_graphs(train_scene_data, vertice_schema, edge_schema)
+train_scene_graphs = parse_scene_graphs(train_scene_data, vertice_schema_with_attr, edge_schema)
 
 # COMMAND ----------
 
-display(scene_graphs.vertices)
+# display(scene_graphs.vertices)
+display(train_scene_graphs.vertices)
 
 # COMMAND ----------
 
-display(scene_graphs.edges)
+# display(scene_graphs.edges)
+display(train_scene_graphs.edges)
 
 # COMMAND ----------
 
@@ -171,12 +216,16 @@ display(scene_graphs.edges)
 
 # COMMAND ----------
 
-print("Objects: {}".format(scene_graphs.vertices.count()))
-print("Relations: {}".format(scene_graphs.edges.count()))
+# print("Objects: {}".format(scene_graphs.vertices.count()))
+# print("Relations: {}".format(scene_graphs.edges.count()))
+
+print("Objects: {}".format(train_scene_graphs.vertices.count()))
+print("Relations: {}".format(train_scene_graphs.edges.count()))
 
 # COMMAND ----------
 
-display(scene_graphs.degrees.sort(["degree"],ascending=[0]).limit(20))
+# display(scene_graphs.degrees.sort(["degree"],ascending=[0]).limit(20))
+display(train_scene_graphs.degrees.sort(["degree"],ascending=[0]).limit(20))
 
 # COMMAND ----------
 
@@ -184,7 +233,9 @@ display(scene_graphs.degrees.sort(["degree"],ascending=[0]).limit(20))
 
 # COMMAND ----------
 
-motifs = scene_graphs.find("(a)-[ab]->(b); (b)-[bc]->(c)")
+# motifs = scene_graphs.find("(a)-[ab]->(b); (b)-[bc]->(c)")
+
+motifs = train_scene_graphs.find("(a)-[ab]->(b); (b)-[bc]->(c)")
 
 display(motifs)
 
